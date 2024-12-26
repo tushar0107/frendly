d41855b442b6f63cde28456b47cc85f4c3da2bec
tushar0107 <tushardasare1701@gmail.com>
2024-12-26 18:20:46 +0530

    component for change password has been made

--- frendly-web/src/modules/Base.jsx
+++ frendly-web/src/modules/Base.jsx
@@ -1,7 +1,9 @@
-import { useContext, useEffect, useState, useTransition } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useContext, useState, useTransition } from "react";
+import { Link } from "react-router-dom";
 import {AppContext} from '../Context';
 import { useSelector } from "react-redux";
+import axios from "axios";
+import { apiurl } from "../components/assets";
 const {name} = require('../../package.json');
 const appName = name;
 
@@ -17,6 +19,20 @@ const Header = ()=>{
 				<img src="/assets/live-chat.png" alt="" />
 			</Link>:null}
 		</header>
+	);
+}
+
+const HeadBar = ({title,detail})=>{
+	return(
+	<header id="headbar">
+		<div id="head">
+			<button onClick={()=>window.history.back()}><img src="/assets/back.png" className="tiny-icon" alt="" ></img></button>
+			<p>
+				<span>{title}</span>
+				{detail?<span className="name">{detail?.first_name+' '+detail.last_name}</span>:null}
+			</p>
+		</div>
+	</header>
 	);
 }
 
@@ -37,4 +53,50 @@ const Footer = ()=>{
 	);
 }
 
-export {Footer, Header};
+const ChangePassword = ()=>{
+	const [pass,setPass] = useState('');
+	const [confPass,setConfPass] = useState('');
+	const [passShow,setPassShow] = useState(false);
+	const [confPassShow,setConfPassShow] = useState(false);
+	const {setIsloading} = useContext(AppContext);
+	const user = useSelector(state=>state.user.user);
+
+	const passChange = (action)=>{
+		if(pass && confPass){
+			setIsloading(true);
+			axios.post(apiurl+action,{'username':user.username,'oldPass':pass,'newPass':confPass}).then((res)=>{
+				if(res.data.status){
+					setIsloading(false);
+					alert(res.data.status);
+				}
+			}).catch(e=>{
+				setIsloading(false);
+				console.log()
+			});
+		}
+	}
+
+	return (
+		<div id="login-modal" className="">
+			<form id="change-password" className="auth-container">
+				<h2>Change Password</h2>
+				<label htmlFor="old-password">
+					<input type={passShow?"text":"password"} value={pass} onChange={(e)=>setPass(e.target.value)}></input>
+					<span style={pass?{top:'0px',left: '.3rem',fontSize: '0.8rem'}:null}>Enter Current Password</span>
+					<img src={`/assets/${passShow?'hide.png':'show.png'}`} onClick={()=>setPassShow(!passShow)} className="pass-show-hide" alt="" />
+					</label>
+				<label htmlFor="new-password">
+					<input type={confPassShow?"text":"password"} value={confPass} onChange={(e)=>setConfPass(e.target.value)}></input>
+					<span style={confPass?{top:'0px',left: '.3rem',fontSize: '0.8rem'}:null}>Enter New Password</span>
+					<img src={`/assets/${confPassShow?'hide.png':'show.png'}`} onClick={()=>setConfPassShow(!confPassShow)} className="pass-show-hide" alt="" />
+				</label>
+				<div className="submit">
+					<button type="button" className="submit-btn btn" onClick={()=>{passChange('change-password')}}>Submit</button>|
+					<button onClick={()=>{window.history.back()}} type="button" className="submit-btn btn">Cancel</button>
+				</div>
+			</form>
+		</div>
+	);
+}
+
+export {Footer, Header, HeadBar, ChangePassword};
