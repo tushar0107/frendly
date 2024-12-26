f9fe12c176d59545c3d830383e20281650eaf342
tushar0107 <tushardasare1701@gmail.com>
2024-12-26 18:22:47 +0530

    made update in database configurations

--- frenly-server/connection.js
+++ frenly-server/connection.js
@@ -89,8 +89,8 @@ app.post('/api/v1/login',function(req,res){
                 'error':err.message,
                 'message':'Database error in logging in'
             });
-        }else if(user?.rows){
-            client.query(`SELECT * FROM posts WHERE author_id=${user.rows[0].id};`,(err,posts)=>{
+        }else if(user.rows.length){
+            client.query(`SELECT p.*,l.count AS like_count,u.first_name,u.last_name,u.username FROM posts p LEFT JOIN likes_count l ON p.id=l.post_id LEFT JOIN users u ON p.author_id=u.id WHERE author_id=${user.rows[0].id};`,(err,posts)=>{
                 if(err){
                     res.status(200).json({
                         'status':true,
@@ -106,6 +106,11 @@ app.post('/api/v1/login',function(req,res){
                     });
                 }
             });
+        }else{
+            res.status(200).json({
+                'status':false,
+                'message':'Incorrect Username'
+            });           
         }
     });
 });
@@ -129,7 +134,7 @@ app.post('/api/v1/signup',function (req,res) {
                     'message':'Username already exists'
                 });
             }else{
-                client.query(`INSERT INTO users(first_name,last_name,username,password) VALUES('${formdata.first_name || ''}','${formdata.last_name || ''}','${formdata.username}','${formdata.password}')`,async(err,result)=>{
+                client.query(`INSERT INTO users(first_name,last_name,username,password) VALUES('${formdata.first_name}','${formdata.last_name}','${formdata.username}','${formdata.password}')`,async(err,result)=>{
                     if(err){
                         res.status(500).json({
                             'status':false,
@@ -141,9 +146,8 @@ app.post('/api/v1/signup',function (req,res) {
                         if(result?.rows.length==0){
                             res.status(200).json({
                                 'status':true,
-                                'data':formdata,
-                                'posts':[],
-                                'message':'Registered successfully'
+                                'data':result.rows,
+                                'message':'Registered successfully. Please login'
                             });
                         }
                     }
@@ -158,7 +162,13 @@ app.post('/api/v1/signup',function (req,res) {
 app.post('/api/v1/update-details',(req,res)=>{
     const formdata = req.body;
     const {username} = formdata;
-    client.query(``,(err,result)=>{
+    client.query(`
+        UPDATE users SET ${formdata.username? `username=${formdata.username},`: ''}
+                         ${formdata.first_name? `first_name=${formdata.first_name},`:''}
+                         ${formdata.last_name? `last_name=${formdata.last_name},`:''} 
+                         ${formdata.password? `password=${formdata.password},`:''}
+                         1=1 WHERE id=${formdata.id};
+        `,(err,result)=>{
         if(err){
             res.status(500).json({
                 'status':false,
@@ -189,7 +199,7 @@ app.post('/api/v1/refresh-profile',(req,res)=>{
                 'message':'Database error in fetching user data'
             });
         }else{
-            client.query(`SELECT * FROM posts WHERE author_id=${user_id};`,(err,posts)=>{
+            client.query(`SELECT p.*,l.count AS like_count,u.first_name,u.last_name,u.username FROM posts p LEFT JOIN likes_count l ON p.id=l.post_id LEFT JOIN users u ON p.author_id=u.id WHERE author_id=${user_id};`,(err,posts)=>{
                 if(err){
                     res.status(500).json({
                         'status':false,
@@ -211,73 +221,80 @@ app.post('/api/v1/refresh-profile',(req,res)=>{
 });
 //SELECT u.id,u.username,u.first_name,u.last_name, p.* FROM users u LEFT JOIN posts p ON u.id=p.author_id WHERE u.id=${user_id};
 // create post api
-app.post('/api/v1/create-post',upload.any('post_content'),(req,res)=>{
+app.post('/api/v1/create-post',upload.any('post_content'),async(req,res)=>{
     const post = req.body.post;
-    post.post_content = req.files.map((file)=>{return '/post-files/'+file.filename});
-    const sqlquery = `
-        INSERT INTO posts(author_id,post_content,caption,hashtags,location,created_at) 
-        VALUES($1, $2, $3, $4, $5, $6);`;
-        const values = [
-            post.user_id,
-            req.files.map((f)=>{return `/post-files/${f.filename.toString()}`}),
-            post.caption,
-            post.hashtags.map((h)=>{return h}),
-            post.location,
-            post.time,
-        ]
-    client.query(sqlquery,values,(err,result)=>{
-        if(err){
-            res.status(500).json({
-                'status':false,
-                'data':null,
-                'error':err,
-                'message':'Database error'
-            });
-        }else{
-            if(result?.rows){
-                res.status(200).json({
-                    'status':true,
-                    'data':result.rows,
-                    'message':'Post Created successfully'
-                });
-            }
-        }
-    });
+    post.post_content = req.files.map((file)=>{return `/post-files/${file.filename.toString()}`});
+    const values = [
+        post.user_id,
+        post.post_content,
+        post.caption,
+        post.hashtags?.map((h)=>{return h}),
+        post.location
+    ];
+    await client.query('BEGIN;');
+    const newPost = await client.query(`INSERT INTO posts(author_id,post_content,caption,hashtags,location) VALUES($1, $2, $3, $4, $5) RETURNING id;`,values);
+    const post_id = newPost.rows[0].id;
+    await client.query(`INSERT INTO likes_count (post_id,count) VALUES (${post_id}, 0);`);
+    const result = await client.query('COMMIT;');
+    if(result){
+        res.status(200).json({
+            'status':true,
+            'message':'Post Created successfully'
+        });
+    }else{
+        res.status(200).json({
+            'status':false,
+            'data':null,
+            'message':'Server Error in creating post'
+        });
+    }
 });
 
+// from frontend if post is already liked
+// >>> delete query in the likes table and update query in the posts table to decrement count
+// from frontend if post is not liked
+// >>> insert query with on conflict do nothing and update query in the posts table to increment count
 // api to add a like to a post
-app.post('/api/v1/like-post',(req,res)=>{
-    const {post_id,user_id,like} = req.body.data;
+app.post('/api/v1/like-post',async(req,res)=>{
+    const {post_id,user_id,like} = req.body;
     if(like===true){
-        client.query(`INSERT INTO likes(post_id,user_id) VALUES(${post_id,user_id});`,(err,result)=>{
-            if(err){
-                res.status(500).json({
-                    'status':false,
-                    'error':err,
-                    'message':'Database error'
-                });
-            }else{
-                res.status(500).json({
-                    'status':true,
-                    'data':null,
-                });
-            }
-        });
+        //to like the post
+        await client.query('BEGIN;');
+        await client.query(`INSERT INTO likes(post_id,user_id) VALUES(${post_id},${user_id}) ON CONFLICT DO NOTHING;`);
+        await client.query(`UPDATE posts SET like_count=like_count+1 WHERE id=${post_id};`);
+        const result = await client.query('COMMIT;');
+        if(result){
+            res.status(200).json({
+                'status':true,
+                'data':null,
+                'message':'liked'
+            });
+        }else{
+            res.status(200).json({
+                'status':false,
+                'data':null,
+                'message':'Error in liking post'
+            });
+        }
     }else if(like===false){
-        client.query(`DELETE FROM likes WHERE post_id=${post_id} AND user_id=${user_id});`,(err,result)=>{
-            if(err){
-                res.status(500).json({
-                    'status':false,
-                    'error':err,
-                    'message':'Database error'
-                });
-            }else{
-                res.status(500).json({
-                    'status':true,
-                    'data':null,
-                });
-            }
-        });
+        // to unlike the post
+        await client.query('BEGIN;');
+        await client.query(`DELETE FROM likes WHERE post_id = ${post_id} AND user_id = ${user_id};`);
+        await client.query(`UPDATE posts SET like_count=GREATEST(like_count-1,0) WHERE id=${post_id};`);
+        const result = await client.query('COMMIT;');
+        if(result){
+            res.status(200).json({
+                'status':true,
+                'data':null,
+                'message':'unliked'
+            });
+        }else{
+            res.status(500).json({
+                'status':false,
+                'error':err.message,
+                'message':'Database error'
+            });
+        }
     }
 });
 
@@ -300,11 +317,40 @@ app.post('/api/v1/comment-post',(req,res)=>{
     });
 });
 
+//api for feed posts
+app.post('/api/v1/feed-posts',(req,res)=>{
+    const {preferences,user_id} = req.body;
+    client.query(`
+                SELECT u.id,u.username, 
+                        p.*,
+                        l.user_id AS liked_acc_id
+                FROM posts p
+                INNER JOIN users u ON u.id=p.author_id
+                LEFT JOIN likes l ON p.id=l.post_id AND l.user_id=${user_id} 
+                ORDER BY p.created_at DESC;
+                `,(err,result)=>{
+        if(err){
+            res.status(500).json({
+                'status':false,
+                'error':err.message,
+                'message':'Database error'
+            });
+        }else{
+            res.status(200).json({
+                'status':true,
+                'data':result.rows
+            });
+        }
+    });
+});
+
+// WHERE p.created_at >= NOW() - INTERVAL '2 days'
 // to get posts on search based on search queries or for feeds based on user's preferences
 // search: 'any array of string', category: 'post' / 'account' / 'videos' / 'tags
 app.post('/api/v1/search',(req,res)=>{
-    const {search,category} = req.body;
-    client.query(`SELECT u.id,u.username,u.first_name,u.last_name, p.* FROM users u LEFT JOIN posts p ON u.id=p.author_id WHERE u.username='${search}';`,(err,result)=>{
+    const {search} = req.body;
+    // client.query(`SELECT u.id,u.username,u.first_name,u.last_name, p.* FROM users u LEFT JOIN posts p ON u.id=p.author_id WHERE p.caption ~* '.*${preferences}.*';`,(err,result)=>{
+    client.query(`SELECT u.id,u.username, p.*,l.count AS like_count FROM posts p INNER JOIN users u ON u.id=p.author_id LEFT JOIN likes_count l ON p.id=l.post_id WHERE u.username='${search}' OR p.caption ~* '.*${search}.*';`,(err,posts)=>{
         if(err){
             res.status(500).json({
                 'status':false,
@@ -312,45 +358,32 @@ app.post('/api/v1/search',(req,res)=>{
                 'message':'Database error'
             });
         }else{
-            res.status(200).json({
-                'status':true,
-                'data':{
-                    'posts':result.rows,
-                    'accounts':[]
-                },
+            client.query(`SELECT id,username,first_name,last_name FROM users WHERE username ~* '.*${search}.*';`,(err,accounts)=>{
+                if(err){
+                    res.status(500).json({
+                        'status':false,
+                        'error':err,
+                        'message':'Database error'
+                    });
+                }else{    
+                    res.status(200).json({
+                        'status':true,
+                        'data':{
+                            'posts':posts.rows,
+                            'accounts':accounts.rows
+                        },
+                    });
+                }
             });
         }
     });
-    // mongo.then(async(db)=>{
-    //     var posts = posts = await db.collection('posts').find({$text:{$search:search.join(" ")}}).toArray();
-    //     var accounts = accounts = await db.collection('users').find({$text:{$search:search.join(" ")}},{projection:{username:1,_id:1}}).toArray();
-    //     var result = {posts:posts,accounts:accounts};
-    //     if(result.posts.length || result.accounts.length){
-    //         res.status(200).json({
-    //             'status':true,
-    //             'data':result,
-    //             'message':'success'
-    //         });
-    //     }else{
-    //         res.status(200).json({
-    //             'status':false,
-    //             'data':null,
-    //             'message':'Unable to fetch posts'
-    //         });
-    //     }
-    // }).catch(e=>{
-    //     console.log('Error fetching posts: ',e.message);
-    //     res.status(500).json({
-    //         'status':false,
-    //         'message':e.message
-    //     });
-    // });
 });
 
 //searches users to tag into while creating new posts
 app.post('/api/v1/find-tags',(req,res)=>{
-    const re = new RegExp(req.body.search);
-    client.query(`SELECT id,username,first_name,last_name FROM users WHERE username ~* '.*${req.body.search}.*' OR first_name ~* '.*${req.body.search}.*' OR last_name ~* '.*${req.body.search}.*';`,(err,result)=>{
+    const {search} = req.body;
+    const values = [`.*${search}.*`,`.*${search}.*`,`.*${search}.*`];
+    client.query(`SELECT id,username,first_name,last_name FROM users WHERE username ~* $1 OR first_name ~* $2 OR last_name ~* $3;`,values,(err,result)=>{
         if(err){
             res.status(500).json({
                 'status':false,
@@ -370,7 +403,7 @@ app.post('/api/v1/find-tags',(req,res)=>{
 
 // to fetch user deatils 
 app.get('/api/v1/get-user/:username',(req,res)=>{
-    client.query(`SELECT u.id,u.username,u.first_name,u.last_name, p.* FROM users u LEFT JOIN posts p ON u.id=p.author_id WHERE u.username='${req.params.username}';`,(err,result)=>{
+    client.query(`SELECT u.id,u.username,u.first_name,u.last_name, p.*, l.count AS like_count FROM users u LEFT JOIN posts p ON u.id=p.author_id LEFT JOIN likes_count l ON p.id=l.post_id WHERE u.username='${req.params.username}';`,(err,result)=>{
         if(err){
             res.status(500).json({
                 'status':false,
@@ -378,12 +411,12 @@ app.get('/api/v1/get-user/:username',(req,res)=>{
                 'error':err.message,
                 'message':'Database error'
             });
-        }else{
-            var userdata = result.rows[0];
+        }else if(result.rows.length){
+            var {id,first_name,last_name,username} = result.rows[0];
             res.status(200).json({
                 'status':true,
-                'data':{id:userdata.id,first_name:userdata.first_name,last_name:userdata.last_name,username:userdata.username},
-                'posts':result.rows,
+                'data':{id,first_name,last_name,username},
+                'posts':result.rows[0].author_id? result.rows:[],
                 'message':'success'
             });
         }
@@ -392,7 +425,7 @@ app.get('/api/v1/get-user/:username',(req,res)=>{
 
 //to get contacts or friends
 app.post('/api/v1/get-contacts',(req,res)=>{
-    client.query(`SELECT id,username,first_name,last_name FROM users;`,(err,result)=>{
+    client.query(`SELECT id,username,first_name|| ' ' || last_name AS name FROM users;`,(err,result)=>{
         if(err){
             res.status(500).json({
                 'status':false,
